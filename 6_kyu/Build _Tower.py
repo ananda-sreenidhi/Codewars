5f2e53dbdb74d14e786733bfe09444360ec10918
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-25 23:14:07 +0530

    Delete Build _Tower.py

--- 6_kyu/Build _Tower.py
+++
@@ -1,7 +0,0 @@
-"""
-Build Tower
-https://www.codewars.com/kata/576757b1df89ecf5bd00073b
-"""
-
-def tower_builder(n_floors):
-    return [' '*(n_floors-i-1)+'*'*(2*i+1)+' '*(n_floors-i-1) for i in range(n_floors)]
