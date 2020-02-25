9419d50ca53de437be40baeef73decb75913491f
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-26 00:08:51 +0530

    Create Find_Jon_Snow_Parents.py

---
+++ Beta/Find_Jon_Snow_Parents.py
@@ -0,0 +1,7 @@
+"""
+Find Jon Snow Parents
+https://www.codewars.com/kata/577a7747ec8832ceb6000199
+"""
+
+def jon_snow_parents(dad, mom):
+    return "Jon Snow you deserve the throne" if dad=="Rhaegar Targaryen" and mom=="Lyanna Stark" else "You know nothing, Jon Snow"
