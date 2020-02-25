758d69e2b9ee0f0e13691c085fbabe8da41b7d2a
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-25 22:54:13 +0530

    Create Multiples of 3 or 5.py

---
+++ 6_kyu/Multiples of 3 or 5.py
@@ -0,0 +1,9 @@
+"""
+Multiples of 3 or 5
+https://www.codewars.com/kata/514b92a657cdc65150000006
+"""
+def solution(number):
+#returns sum of multiples of 3 and 5 in the range of given argument.
+    if str(number).isdigit():
+        l = range(number)
+        return sum(filter(lambda x: x%3==0 or x%5==0, l))  
