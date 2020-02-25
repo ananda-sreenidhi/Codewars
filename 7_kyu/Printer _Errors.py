f4ff68670d283201e770234617e8ad4c431f4343
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-25 23:15:22 +0530

    Delete Printer _Errors.py

--- 7_kyu/Printer _Errors.py
+++
@@ -1,7 +0,0 @@
-"""
-Printer Errors
-https://www.codewars.com/kata/56541980fa08ab47a0000040
-"""
-
-def printer_error(s):
-    return "{}/{}".format(len([i for i in s.lower() if ord(i)>ord('m')]), len(s))
