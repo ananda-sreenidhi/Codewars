f299aa64b2d1207f6b8904f8796ed4c19ef0eb4a
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-25 22:56:11 +0530

    Create IQ _Test.py

---
+++ 6_kyu/IQ _Test.py
@@ -0,0 +1,7 @@
+"""
+IQ Test
+https://www.codewars.com/kata/552c028c030765286c00007d
+"""
+def iq_test(numbers):
+    x = [int(i)%2 for i in numbers.split()]
+    return x.index(1)+1 if sum(x) == 1 else x.index(0)+1
