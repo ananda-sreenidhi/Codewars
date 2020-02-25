0df698cbe8003918366ea964eb30889b5ab42b74
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-25 23:00:03 +0530

    Update Multiples of 3 or 5.py

--- 6_kyu/Multiples of 3 or 5.py
+++ 6_kyu/Multiples of 3 or 5.py
@@ -2,6 +2,7 @@
 Multiples of 3 or 5
 https://www.codewars.com/kata/514b92a657cdc65150000006
 """
+
 def solution(number):
 #returns sum of multiples of 3 and 5 in the range of given argument.
     if str(number).isdigit():
