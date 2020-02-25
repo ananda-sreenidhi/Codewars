e4bd26e9a054fa9026bb6d27d3d25f1efd2863fb
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-02-25 23:05:48 +0530

    Create Printer _Errors.py

---
+++ 7_kyu/Printer _Errors.py
@@ -0,0 +1,7 @@
+"""
+Printer Errors
+https://www.codewars.com/kata/56541980fa08ab47a0000040
+"""
+
+def printer_error(s):
+    return "{}/{}".format(len([i for i in s.lower() if ord(i)>ord('m')]), len(s))
